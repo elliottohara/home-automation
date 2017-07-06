efa300c6902ae74861cd034d3ea8d21842c955e2
Elliott O'Hara <elliott.ohara@gmail.com>
2017-07-06 09:41:47 -0500

    Move nest.js to NestAwayStream and properlly extend EventEmitter

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-let nest = require("./nest");
+"use strict;"
+
+//for now, this file just demonstrates the use of the NestAwayStream
 let config = require('./config');
-nest.events.on('awayOn', () => console.log(`Away on`));
-nest.events.on('awayOff', () => console.log(`Away off`));
+let nestAwayStream = require("./streams/nestAwayStream");
+let nest = new nestAwayStream(config.nest);
+
+nest.connect();
+nest.on('loggedIn', () => console.log('Logged In'));
+nest.on('subscribed', () => console.log('subscribed'));
+nest.on('away', () => console.log(`Away`));
+nest.on('home', () => console.log(`Home`));
---
+++ streams/nestAwayStream.js
@@ -0,0 +1,60 @@
+"use strict";
+let nest = require('unofficial-nest-api')
+let EventEmitter = require('events').EventEmitter;
+let self;
+
+class NestAwayEventEmitter extends EventEmitter{
+    constructor(config){
+        super();
+        this.email = config.Email;
+        this.password = config.Password;   
+        this.subscribed = false; 
+        self = this;
+    }
+
+    connect(){
+        nest.login(this.email, this.password, (err, data) => {
+            if(!err){
+                this.emit('loggedIn', data);
+            }else{
+                this.emit('error', err);
+                return;
+            }
+            nest.fetchStatus( (data) => {
+                let away = data.structure[Object.keys(data.structure)[0]].away
+                self._emitEvents(away);
+                self._subscribe();
+            });
+        });
+    }
+    
+    _emitEvents(away, data){
+        if(away){
+                self.emit('away', data);
+            }else{
+                self.emit('home', data);
+            }
+    }
+
+    _subscribe(){
+        // only care about structure
+        nest.subscribe(
+            (deviceId, data, type) => self._subscribeDone(deviceId, data, type), ['structure']);
+    }
+
+    _subscribeDone(deviceId, data, type) {
+        if(!self.subscribed){
+            self.emit('subscribed', data);
+            self.subscribed = true;
+        }
+
+        if (deviceId) {
+            self._emitEvents(data.away, data);
+        }
+        
+        setTimeout(self._subscribe, 2000);
+    }
+}
+
+
+module.exports = NestAwayEventEmitter;
