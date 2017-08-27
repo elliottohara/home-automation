5fbf8d9f0d8d9a70a5f23b5c6b232eac002cb6a6
Elliott O'Hara <elliott.ohara@gmail.com>
2017-08-26 20:22:03 -0500

    some cleanup

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 "use strict;"
-
-//for now, this file just demonstrates the use of the NestAwayStream
 let config = require('./configs/config');
 //NestStuff();
 WemoStuff();
@@ -17,8 +15,6 @@ function NestStuff(){
   let rule2 = new nestRule('home', new trueMatch(), () => console.log('home'));
   rule2.register(nest);
   
-  
-
   nest.connect();
   
 }
@@ -26,32 +22,16 @@ function NestStuff(){
 function WemoStuff(){
   let wemoStream = require('./streams/wemoStream');
   let wemo = new wemoStream();
-  
-
   let wemoRules = require('./rules/wemoRules');
   let wemoRule = wemoRules.WemoRule;
-  let nameMatches = wemoRules.NameMatches;
-
-  let rule1Callback = (val, breakfastLight) => {
-      let device = wemo.clients.find((c) => c.device.friendlyName === "Breakfast");
-      if(device){
-        device.setBinaryState(val);
-      }
-  };
-  
-  
-  let rule1 = new wemoRule('binaryState', 
-    new nameMatches("Kitchen Overhead Lights"), rule1Callback);
-  
   let rule = new wemoRules.MatchStateWemoRule('Breakfast', wemo);
+  
   wemo.on('deviceDiscovered', (client) => {
     if( client.device.friendlyName === 'Kitchen Overhead Lights') {
       rule.register(client);
     }
-    //rule1.register(client);
-  });
+   });
 
   wemo.connect();
-  
   
 }
